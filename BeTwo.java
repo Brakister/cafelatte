66f0783f1c64ff9ac7c5e3d0953cb5f9e58b7ab4
Brakister <smashbored@gmail.com>
2025-01-14 01:37:23 -0300

    mesma coisa do anterior

--- BeTwo.java
+++ BeTwo.java
@@ -1,13 +1,34 @@
+
+
 public class BeTwo {
     public static void main(String[] args) {
         sum(32, 23);
-        sum(4, 3);
-        sum(3, 2);
-        sum(54, 22);
+        per(23.4, 43);
+        yor("Glory to ", "Mankind");
+        piper(false, " A2 is Alive");
+        conjun(33, 23.7, " Alegações");
     }
 
-    static void sum(int x, int y) {
+    static void sum(int x, int y) { //declaração padrão 
+        System.out.println(x + y);
+        
+    }
+
+    static void per( double x, int y) {
+        System.out.println(x / y);
+        
+
+    }
+
+    static void yor(String x, String y ){
         System.out.println(x + y);
     }
 
+    static void piper(boolean x, String y){
+        System.err.println(x+ y);
+    }
+
+    static void conjun(int x, double y, String z){
+        System.err.println(x+y+z);
+    }
 }
