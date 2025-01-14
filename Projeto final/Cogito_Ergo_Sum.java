b510bf16c8747ae76faea3be00fc0ae8f5ccd527
Brakister <smashbored@gmail.com>
2025-01-14 03:16:26 -0300

    ideias

--- Projeto final/Cogito_Ergo_Sum.java
+++ Projeto final/Cogito_Ergo_Sum.java
@@ -1,6 +1,27 @@
 //Programa que vai recomendar livros com base no que você responder
+// ideias: 
+// 1- Introduzir uma tela interativa com perguntas e respostas estabelecidas;
+// 2- Conter um acervo de pelo menos 20 livros relacionados a filosofia;
+// 3- Com base nas respostas, fazer um filtro que relaciona o que vai ser melhor para o usuário;
+// 4- Talvez um sistema de recomendações pseudoaleatórias;
+// Possiveis perguntas (sujeito a mudança);
+// Você sabe ler? (sim, não); Se pressionado "não" abre uma imagem com um botão de retorno
+// O quanto você lê? (Muito, Pouco);
+// Qual relaciona melhor seu jeito de ler? (Fácil entendimento, Algo para me fazer pensar);
+// Quanto tempo você se dispoe? (<100 pag, >100pag);
+// Deve-se pensar para atingir-se numa reflexão sobre sua própria existencia? (sim, não)
+// Não quero ler nada, na verdade. <botão> (Leva a uma imagem com som)
+// Sou burro demais para formar opinião própria (talvez com uma imagem),
+//(Vai surgir escondido em algum lugar da tela, ao clicar redireciona para um vídeo acho)
+
 
 
 public class Cogito_Ergo_Sum {
+ 
     
+    public static void main(String[] args) {
+        
+    }
 }
+
+    
