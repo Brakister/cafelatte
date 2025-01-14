ec6f4ce2db825c61ed96c4b9e096de7414e5d865
Brakister <smashbored@gmail.com>
2025-01-14 16:58:12 -0300

    olá

---
+++ yorh.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class yorh {
+    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //Para interatividade
+
+        System.err.println("Digite seu nominho: ");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Olá, " + name);
+    
+    }
+}
