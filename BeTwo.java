27f58704f0c1980bc54b51cdc72715d6ecc1fee2
Brakister <smashbored@gmail.com>
2025-01-14 00:56:12 -0300

    soma

---
+++ BeTwo.java
@@ -0,0 +1,13 @@
+public class BeTwo {
+    public static void main(String[] args) {
+        sum(32, 23);
+        sum(4, 3);
+        sum(3, 2);
+        sum(54, 22);
+    }
+
+    static void sum(int x, int y) {
+        System.out.println(x + y);
+    }
+
+}
