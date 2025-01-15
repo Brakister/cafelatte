6ccf509acb92647d7652d84846ee789e9765158a
Brakister <smashbored@gmail.com>
2025-01-14 22:30:52 -0300

    butão

--- Aprot4/src/Screen.java
+++ Aprot4/src/Screen.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Screen extends JFrame { //extends serve pra não precisar ficar colocando jframe em
     // todo  set etc
@@ -11,10 +12,22 @@ public class Screen extends JFrame { //extends serve pra não precisar ficar col
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
-
+        setLayout(null);
         JButton jButton = new JButton();
         jButton.setText("Clique aqui");
+        jButton.setBounds(100, 200, 250, 70);
+        jButton.setFont(new Font("Arial", Font.BOLD, 30));
+        jButton.setForeground(new Color(10, 10, 10));
+        jButton.setBackground(new Color(120, 113, 113));
+
+        JButton jButton2 = new JButton();
+        jButton2.setText("não aqui");
+        jButton2.setBounds(200, 300, 350, 70);
+        jButton2.setFont(new Font("Arial", Font.BOLD, 30));
+        jButton2.setForeground(new Color(10, 10, 10));
+        jButton2.setBackground(new Color(120, 113, 113));
         add(jButton);
+        add(jButton2);
 
     }
 }
